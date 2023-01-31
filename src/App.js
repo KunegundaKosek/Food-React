408ab0f0750326ba6052ddf945295c3cd8ccfd5a
Kunegunda <kosekkuengunda@wp.pl>
2023-01-31 12:26:06 +0100

    Using the Context

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, { Fragment, useState } from "react";
-import Cart from "./components/Cart/Cart";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-
     const [cartIsShown, setCartIsShown] = useState(false);
 
     const showCartHandler = () => {
         setCartIsShown(true);
-        console.log(123)
     };
 
     const hideCartHandler = () => {
         setCartIsShown(false);
-    }
+    };
 
     return (
-        <Fragment>
-
+        <CartProvider>
             {cartIsShown && <Cart onClose={hideCartHandler} />}
-
             <Header onShowCart={showCartHandler} />
             <main>
                 <Meals />
             </main>
-        </Fragment>
+        </CartProvider>
     );
-};
+}
 
 export default App;
